6f887254c1effd8e6d97556e761dc86f31cba1e8
Miguel Pedraza <mikeped469@gmail.com>
2024-01-15 22:05:51 -0600

    Initial commit with tailwind installed and set up with basic styles

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+// Styles import
+import "./globals.css";
+
+const App = () => {
+	return <h1 className="text-3xl font-bold underline">App</h1>;
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+// React dom imports
+import ReactDOM from "react-dom/client";
+
+// Components
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
