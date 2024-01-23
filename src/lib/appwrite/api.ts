85e1475d51edca6a40d4e8fa89d32a93ee10af8e
Miguel Pedraza <mikeped469@gmail.com>
2024-01-23 16:04:20 -0600

    Added configuration for appwrite, api that uses appwrite config and types file

---
+++ src/lib/appwrite/api.ts
@@ -0,0 +1,22 @@
+// Appwrite imports
+import { ID } from "appwrite";
+import { account } from "./config";
+
+// Types imports
+import { INewUser } from "@/types";
+
+export async function createUserAccount(user: INewUser) {
+	try {
+		const newAccount = await account.create(
+			ID.unique(),
+			user.email,
+			user.password,
+			user.name
+		);
+
+		return newAccount;
+	} catch (error) {
+		console.log(error);
+		return error;
+	}
+}
