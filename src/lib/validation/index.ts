6ee8f71d3a2dacc243b01cd9eb432fb89590d221
Miguel Pedraza <mikeped469@gmail.com>
2024-01-23 15:44:25 -0600

    Added all assets and initial sign up component using shadcn form components that adds react hook forms and zod

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,12 @@
+// Zod imports
+import * as z from "zod";
+
+// Form schema
+export const SignupValidation = z.object({
+	name: z.string().min(2, { message: "Too short" }),
+	username: z.string().min(2, { message: "Too short" }),
+	email: z.string().email(),
+	password: z.string().min(8, {
+		message: "Password must be at least 8 characters",
+	}),
+});
