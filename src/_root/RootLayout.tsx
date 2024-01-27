fbfa2eba4e36a4bccb42fd20b48addf12cbe7ecc
Miguel Pedraza <mikeped469@gmail.com>
2024-01-27 17:02:05 -0600

    Added skeleton of hoe layout is going to be divided (created components)

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,7 +1,24 @@
-const RootLayout = () => {
-  return (
-    <div>RootLayout</div>
-  )
-}
+// React rotuer dom imports
+import { Outlet } from "react-router-dom";
 
-export default RootLayout
+// Components
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+
+const RootLayout = () => {
+	return (
+		<div className="w-full md:flex">
+			<Topbar />
+			<LeftSidebar />
+
+			<section className="flex flex-1 h-full">
+				<Outlet />
+			</section>
+
+			<Bottombar />
+		</div>
+	);
+};
+
+export default RootLayout;
