8b483f029471e90514a210410bf25415a4a1741d
Miguel Pedraza <mikeped469@gmail.com>
2024-01-27 17:18:18 -0600

    Added the topbar for mobile view and added sign out functionality with mutation and appwrite api function

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,7 +1,58 @@
-const Topbar = () => {
-  return (
-    <div>Topbar</div>
-  )
-}
+// React imports
+import { useEffect } from "react";
 
-export default Topbar
+// React rotuer dom imports
+import { Link, useNavigate } from "react-router-dom";
+
+// Context
+import { useUserContext } from "@/context/AuthContext";
+
+// Mutations and queries
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
+
+// Shadcn components
+import { Button } from "@/components/ui/button";
+
+const Topbar = () => {
+	const navigate = useNavigate();
+	const { mutate: signOut, isSuccess } = useSignOutAccount();
+	const { user } = useUserContext();
+
+	useEffect(() => {
+		if (isSuccess) navigate(0);
+	}, [isSuccess]);
+
+	return (
+		<section className="topbar">
+			<div className="flex-between py-4 px-5">
+				<Link to="/" className="flex gap-3 items-center">
+					<img
+						src="/assets/images/logo.svg"
+						alt="logo"
+						width={130}
+						height={325}
+					/>
+				</Link>
+
+				<div className="flex gap-4">
+					<Button
+						variant="ghost"
+						className="shad_button_ghost"
+						onClick={() => signOut()}
+					>
+						<img src="/assets/icons/logout.svg" alt="logout" />
+					</Button>
+					<Link to={`/profile/${user.id}`} className="flex-center gap-3">
+						<img
+							src={user.imageUrl || "/assets/images/profile-placeholder.svg"}
+							alt="profile"
+							className="h-8 w-8 rounded-full"
+						/>
+					</Link>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Topbar;
